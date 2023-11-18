60fcf4f9ede016f13fae2dda1bffb3172eabbb0c
marcelopaivag <mff.paiva@gmail.com>
2023-11-18 13:12:23 -0300

    final de la clase

--- src/components/Routes/AuthRoute.js
+++ src/components/Routes/AuthRoute.js
@@ -23,7 +23,7 @@ export default function AuthRoute({ component: Component, ...props }) {
             if(loading) return null
 
             return authStatus ? 
-                (<Redirect to="/client/" />)
+                (<Redirect to="/client" />)
                 :
                 (<Component {...props} />)
             }

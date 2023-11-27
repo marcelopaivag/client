428a83a3647ed54166b7d156c3c4bcce2b368866
marcelopaivag <mff.paiva@gmail.com>
2023-11-27 15:10:18 -0300

    logo omtstore

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
           <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
+                <img src="https://res.cloudinary.com/dbteftlej/image/upload/v1701107797/omtstorelogo_grnm0n.png" class="h-8 me-3" alt="FlowBite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">OMTstore</span>
               </a>
             </div>
